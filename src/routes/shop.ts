7b186e284373aff7831f13181b5b796a203c4ccf
Anthony Infortunio <contact@anthonygleason.com>
2023-10-27 22:00:39 -0400

    .

--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -517,6 +517,7 @@ shopRouter.post('/carts/create-payment-intent',authenticateCartToken,authenticat
     if (!pendingOrderDoc) throw new Error('A pending order doc was not found!');
     if (!cart || cart.isCartEmpty()) throw new Error('You cannot proceed to checkout with an empty cart.');
     const finalPriceInCents:number = Math.floor(cart.finalPrice * 100);
+    console.log('create payment intent pending order doc', pendingOrderDoc);
     //create payment intent
     const paymentIntent = await stripe.paymentIntents.create({
       amount: finalPriceInCents,
